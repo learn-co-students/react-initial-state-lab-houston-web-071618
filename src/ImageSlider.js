c8db59f3f6fc347eec9b9d5f622612fefbf7865e
Alejandro Aguilar Jr <alejandro.aguilar.jr@gmail.com>
2018-09-18 11:04:05 -0500

    Done.

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,24 @@
 // your ImageSlider code here!
+import React, { Component } from 'react';
+
+
+export default class ImageSlider extends Component {
+
+  constructor(props) {
+    super(props)
+    // ...define initial state with a key of 'color' set to the 'value' prop
+    // currentSlideIndex that starts at 0
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+
+   render() {
+     return (
+       <div>
+        I am on slide {this.state.currentSlideIndex}
+       </div>
+     )
+   }
+
+}
